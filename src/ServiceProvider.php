eabe90b420cd7a18e04c8a6893cbd2baee0a6c20
ElseZhang <mzhang173@gmail.com>
2018-09-12 11:11:38 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the elsezhang/weather.
+ *
+ * (c) ElseZhang <mzhang173@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace ElseZhang\Weather;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
@@ -8,7 +17,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     public function register()
     {
-        $this->app->singleton(Weather::class, function(){
+        $this->app->singleton(Weather::class, function () {
             return new Weather(config('services.weather.key'));
         });
 
